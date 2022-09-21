bb8aca02e74b46fc6ac3149cbc7408e70a05c51b
Hornakkan <lacour.s@evhr.net>
2022-09-21 11:13:08 +0200

    References card slider

--- script/script.js
+++ script/script.js
@@ -1,17 +1,31 @@
 let carouselInner = document.querySelector('#cardSliderInner');
-let carouselItem = document.querySelector('#cardSliderItem');
 
 let carouselPrev  =document.querySelector('#cardSliderPrev');
 let carouselNext  =document.querySelector('#cardSliderNext');
 
 carouselPrev.addEventListener('click', () => {
     let firstChild = carouselInner.firstElementChild;
+    let carouselItems = document.querySelectorAll('#cardSliderItem');
+
+    carouselItems.forEach(carouselItem => {
+        carouselItem.classList.remove('active')
+    });
+
     carouselInner.firstElementChild.remove();
     carouselInner.appendChild(firstChild);
+    carouselInner.firstElementChild.classList.add('active');
 })
 
 carouselNext.addEventListener('click', () => {
     let lastChild = carouselInner.lastElementChild;
+    
+    let carouselItems = document.querySelectorAll('#cardSliderItem');
+
+    carouselItems.forEach(carouselItem => {
+        carouselItem.classList.remove('active')
+    });
+
     carouselInner.lastElementChild.remove();
     carouselInner.insertBefore(lastChild, carouselInner.firstElementChild);
+    carouselInner.lastElementChild.classList.add('active');
 })
